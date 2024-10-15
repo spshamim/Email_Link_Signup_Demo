3fa40712b2c4a66d3211cc1e12406d0eaa0a6b48
Shahriar Parvez Shamim <spshamim90@gmail.com>
2024-10-16 00:58:46 +0600

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFa-UBHVZVBpqQmeD3LwdJmhXopbVNz5E",
-  authDomain: "email-link-signin-65a2c.firebaseapp.com",
-  projectId: "email-link-signin-65a2c",
-  storageBucket: "email-link-signin-65a2c.appspot.com",
-  messagingSenderId: "625500573517",
-  appId: "1:625500573517:web:a70063e081b5ba8b81f104"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId
 };
 
 const app = initializeApp(firebaseConfig);
